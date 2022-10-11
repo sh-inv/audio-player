22a56143f6cf2149746247ff19f27ca693166147
Goohyun3436 <rngus3436@g.kmou.ac.kr>
2022-10-12 02:59:53 +0900

    Add: 전체 틀 잡기 및 반응형 레이아웃 잡기 완료

--- src/pages/RecordScreen/index.jsx
+++ src/pages/RecordScreen/index.jsx
@@ -1,5 +1,59 @@
+import styled from 'styled-components';
+import AudioList from '../../components/AudioList';
+import AudioWaveForm from '../../components/AudioWaveForm';
+import AudioPlayBar from '../../components/AudioPlayBar';
+import AudioDownLoad from '../../components/AudioDownLoad';
+import AudioRecord from '../../components/AudioRecord';
+
 const RecordScreen = () => {
-  return <div>녹음페이지입니다.</div>;
+  return (
+    <RecordScreenWrapper>
+      <div className='audio-list-content'>
+        <AudioList />
+      </div>
+      <div className='audio-detail-content'>
+        <AudioWaveForm />
+        <AudioPlayBar />
+        <AudioDownLoad />
+        <div className='record-content'>
+          <AudioPlayBar />
+          <AudioRecord />
+        </div>
+      </div>
+    </RecordScreenWrapper>
+  );
 };
 
 export default RecordScreen;
+
+const RecordScreenWrapper = styled.div`
+  display: flex;
+
+  .audio-list-content,
+  .audio-detail-content {
+    display: flex;
+    justify-content: center;
+    margin: 5px;
+    padding: 5px;
+  }
+
+  .audio-list-content {
+    width: 35%;
+    background-color: #d6d641;
+  }
+
+  .audio-detail-content {
+    flex-direction: column;
+    width: 65%;
+    background-color: beige;
+  }
+
+  @media screen and (max-width: 890px) {
+    flex-direction: column;
+
+    .audio-list-content,
+    .audio-detail-content {
+      width: 100%;
+    }
+  }
+`;
--- src/pages/playScreen/index.jsx
+++ src/pages/playScreen/index.jsx
@@ -1,5 +1,54 @@
+import styled from 'styled-components';
+import AudioList from '../../components/AudioList';
+import AudioWaveForm from '../../components/AudioWaveForm';
+import AudioPlayBar from '../../components/AudioPlayBar';
+import AudioDownLoad from '../../components/AudioDownLoad';
+
 const PlayScreen = () => {
-  return <div>play페이지입니다.</div>;
+  return (
+    <PlayScreenWrapper>
+      <div className='audio-list-content'>
+        <AudioList />
+      </div>
+      <div className='audio-detail-content'>
+        <AudioWaveForm />
+        <AudioPlayBar />
+        <AudioDownLoad />
+      </div>
+    </PlayScreenWrapper>
+  );
 };
 
 export default PlayScreen;
+
+const PlayScreenWrapper = styled.div`
+  display: flex;
+
+  .audio-list-content,
+  .audio-detail-content {
+    display: flex;
+    justify-content: center;
+    margin: 5px;
+    padding: 5px;
+  }
+
+  .audio-list-content {
+    width: 35%;
+    background-color: #d6d641;
+  }
+
+  .audio-detail-content {
+    flex-direction: column;
+    width: 65%;
+    background-color: beige;
+  }
+
+  @media screen and (max-width: 890px) {
+    flex-direction: column;
+
+    .audio-list-content,
+    .audio-detail-content {
+      width: 100%;
+    }
+  }
+`;
