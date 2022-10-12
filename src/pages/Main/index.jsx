8b611b63e5cc62506519a98eebe62ef7a503ac6d
sh.inv <104422865+sh-inv@users.noreply.github.com>
2022-10-12 18:09:01 +0900

    Add: 음성녹음 기능 및 UI 구현 (#6)

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -4,7 +4,7 @@ import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import AudioList from '../../components/AudioList';
 import AudioPlay from '../../components/AudioPlay';
-import AudioRecord from '../../components/AudioRecord';
+import AudioRecord from '../../components/AudioRecord/index.jsx';
 import axios from 'axios';
 
 const Main = () => {
