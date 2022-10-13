8be021623baa370ce6020a8a25256a851a7bb322
KOWOONLEE <108816777+KOWOONLEE@users.noreply.github.com>
2022-10-13 20:35:19 +0900

    UI: 다운로드 버튼 및 기타 사이즈 수정 (#16)
    * UI: download bar ui 수정
    
    * UI: 버튼 색상 및 기타 사이즈 수정
    
    Co-authored-by: 이고운 <kowoon@igoun-ui-MacBookAir.local>

--- src/components/AudioPlay/Download/index.jsx
+++ src/components/AudioPlay/Download/index.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { useEffect, useState } from 'react';
+import { FiDownload } from 'react-icons/fi';
 
 const Download = ({ track }) => {
   const [trackUrl, setTrackUrl] = useState('');
@@ -16,14 +17,19 @@ const Download = ({ track }) => {
   return (
     <DownloadWrapper>
       <AudioTypeBox>
-        <p>오디오 파일 확장자를 선택하세요.</p>
+        <p>오디오 파일 확장자를 선택하고 다운로드 받으세요.</p>
         <select name='' id='' onChange={e => setAudioType(e.target.value)} value={audioType}>
           <option value='mp3'>mp3 (default)</option>
           {/* <option value='wav'>wav</option> */}
           {/* <option value='webm'>webm</option> */}
         </select>
+        <p className='down-button'>
+          {/* <DownloadButton href={trackUrl}>
+            <FiDownload />
+          </DownloadButton> */}
+          <DownloadButton href={trackUrl}>오디오파일 다운로드</DownloadButton>
+        </p>
       </AudioTypeBox>
-      <DownloadButton href={trackUrl}>Download</DownloadButton>
     </DownloadWrapper>
   );
 };
@@ -32,7 +38,9 @@ export default Download;
 
 const DownloadWrapper = styled.div`
   display: flex;
-  justify-content: space-between;
+  border: 4px solid;
+  border-image: linear-gradient(0deg, #f0ff00 20%, #58cffb 80%);
+  border-image-slice: 1;
 `;
 
 const AudioTypeBox = styled.div`
@@ -40,17 +48,40 @@ const AudioTypeBox = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 40%;
+  text-align: center;
+  width: 100%;
+  margin: 10px;
+
+  select {
+    width: 28vw;
+    height: 5vh;
+    text-align: center;
+    border: none;
+  }
 
   p {
     margin-bottom: 10px;
   }
+
+  .down-button {
+    width: 180px;
+    margin-top: 10px;
+    padding: 10px;
+    border: 1px solid #000000;
+    border-radius: 10px;
+  }
 `;
 
 const DownloadButton = styled.a`
-  display: flex;
   justify-content: center;
-  width: 60%;
-  padding: 20px 0;
-  background-color: white;
+  width: 20px;
+  height: 20px;
+  padding-left: 3px;
+  bottom: 0;
+  color: black;
+
+  svg {
+    width: 23px;
+    height: 17px;
+  }
 `;
